834a0fd9e4c4ae7d220f15fe1bc31da672865626
Aleksandra Shumkova <a.shumkova@live.com>
2019-09-09 22:18:12 +0700

    исправляет ошибки в валидаторе, добавляет regex для email

--- source/js/app.js
+++ source/js/app.js
@@ -19,23 +19,18 @@ var ESC_KEYCODE = 27;
 
 // Модальные окна
 
-var modalCity = document.querySelector('.modal--city');
+// var modalCity = document.querySelector('.modal--city');
 var modalQuestion = document.querySelector('.modal--question');
 var nameInput = modalQuestion.querySelector('[name=name]');
 var emailInput = modalQuestion.querySelector('[name=email]');
-var questionInput = modalQuestion.querySelector('[name=question]');
 var agreementInput = modalQuestion.querySelector('[name=agreement]');
 var questionForm = modalQuestion.querySelector('form');
-
-var nameTip = modalQuestion.querySelector('.modal__tip--name');
-var emailTip = modalQuestion.querySelector('.modal__tip--email');
-
 var questionSubmitButton = modalQuestion.querySelector('[type=submit]');
+var mdTriggers = document.querySelectorAll('.md-trigger');
+var questionInputs = modalQuestion.querySelectorAll('.js-input');
 
 var isStorageSupport = true;
 var storage = '';
-
-var mdTriggers = document.querySelectorAll('.md-trigger');
 
 try {
   storage = localStorage.getItem('login');
@@ -59,11 +54,6 @@ var removeOverlay = function () {
 };
 
 var overlay = createOverlay();
-
-// var closeModal = function () {
-//   document.querySelector('.modal--show').classList.remove('modal--show');
-//   removeOverlay();
-// };
 
 mdTriggers.forEach(function (el) {
   var modal = document.querySelector('.modal--' + el.getAttribute('data-modal'));
@@ -98,7 +88,6 @@ mdTriggers.forEach(function (el) {
 
 
 if (modalQuestion) {
-
   agreementInput.addEventListener('input', function () {
     if (agreementInput.checked === true) {
       questionSubmitButton.removeAttribute('disabled');
@@ -107,40 +96,13 @@ if (modalQuestion) {
     }
   });
 
-  nameInput.addEventListener('input', function (evt) {
-    evt.preventDefault();
-    if (nameInput.value.length > 0) {
-      if (!nameTip.classList.contains('visible')) {
-        nameTip.classList.add('visible');
-      }
-    } else {
-      nameTip.classList.remove('visible');
-    }
-  });
-
-  emailInput.addEventListener('input', function (evt) {
-    evt.preventDefault();
-    if (emailInput.value.length > 0) {
-      if (!emailTip.classList.contains('visible')) {
-        emailTip.classList.add('visible');
-      }
-    } else {
-      emailTip.classList.remove('visible');
-    }
-  });
-
   questionSubmitButton.addEventListener('click', function () {
-    nameInput.addEventListener('invalid', function (evt) {
-      evt.preventDefault();
-      questionForm.classList.add('submit-focused');
-    });
+    questionForm.classList.add('submit-focused');
 
-    emailInput.addEventListener('invalid', function (evt) {
-      evt.preventDefault();
-    });
-
-    questionInput.addEventListener('invalid', function (evt) {
-      evt.preventDefault();
+    questionInputs.forEach(function (input) {
+      input.addEventListener('invalid', function (evt) {
+        evt.preventDefault();
+      });
     });
   });
 
